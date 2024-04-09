c65eecafd4104297f837834a4fc52d498429d1fc
Arthur Oliveira <arthur_mo@hotmail.com>
2024-04-08 21:12:06 -0300

    Broadcast + Subscription to Topics

--- receiver.py
+++ receiver.py
@@ -7,26 +7,28 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost")
 channel = connection.channel()
 
 channel.exchange_declare(exchange="newsletterTopic", exchange_type="topic")
+channel.exchange_declare(exchange="newsletterBroadcast", exchange_type="fanout")
 
 result = channel.queue_declare("", exclusive=True)
 queue_name = result.method.queue
 
-binding_keys = sys.argv[1:]
-if not binding_keys:
-    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
-    sys.exit(1)
+binding_keys = input("[x] Which topics to subscribe to? Separate with spaces.\n")
 
-for binding_key in binding_keys:
+for binding_key in binding_keys.split(" "):
     if binding_key not in possible_topics:
-        print(f"Topic must be one of these: {', '.join(possible_topics)}")
+        print(f"[x] Topic must be one of these: {', '.join(possible_topics)}")
         sys.exit(1)
     channel.queue_bind(exchange="newsletterTopic", queue=queue_name, routing_key=binding_key)
 
-print("[*] Waiting for logs. To exit press CTRL+C")
+channel.queue_bind(exchange="newsletterBroadcast", queue=queue_name)
+
+print("[x] Waiting for logs. To exit press CTRL+C.")
 
 def callback(ch, method, properties, body):
-    print(f"[x] Received on topic {method.routing_key}: {body.decode()}")
+    if method.routing_key == "broadcast":
+        print(f"[x] Received on broadcast: {body.decode()}")
+    else:
+        print(f"[x] Received on topic {method.routing_key}: {body.decode()}")
 
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
 channel.start_consuming()
--- sender.py
+++ sender.py
@@ -2,17 +2,23 @@ import pika
 import sys
 
 possible_topics = {"cinema", "esportes", "saude", "politica", "broadcast"}
-routing_key = sys.argv[1] if len(sys.argv) > 2 else "broadcast"
+routing_key = input("[x] Which topic to publish to?\n")
 if (routing_key not in possible_topics):
-    print(f"Topic must be one of these: {', '.join(possible_topics)}")
+    print(f"[x] Topic must be one of these: {', '.join(possible_topics)}")
     sys.exit(1)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
 channel = connection.channel()
+message = input(f"[x] Which message to send on topic {routing_key}?\n")
 
-channel.exchange_declare(exchange="newsletterTopic", exchange_type="topic")
+if(routing_key == "broadcast"):
+    channel.exchange_declare(exchange="newsletterBroadcast", exchange_type="fanout")
+    channel.basic_publish(exchange="newsletterBroadcast", routing_key="broadcast", body=message)
+    print(f"[x] Sent on broadcast: {message}")
 
-message = " ".join(sys.argv[2:]) or "Standard message"
-channel.basic_publish(exchange="newsletterTopic", routing_key=routing_key, body=message)
-print(f"[x] Sent on topic {routing_key}: {message}")
+else:
+    channel.exchange_declare(exchange="newsletterTopic", exchange_type="topic")
+    channel.basic_publish(exchange="newsletterTopic", routing_key=routing_key, body=message)
+    print(f"[x] Sent on topic {routing_key}: {message}")
+
 connection.close()
